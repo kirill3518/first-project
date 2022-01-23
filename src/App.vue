ed80a32213e985c572bb1090a45e6cb50f9de4cf
kirill3518 <kirill3518@mail.ru>
2022-01-23 15:07:36 +0500

    ДЗ № 2.

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
 <template>
   <div id="app">
-    <div class="display">
-      <input v-model.number="operand1" />
-      <input v-model.number="operand2" />
-      = {{ result }}
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <div class="keyboard">
-      <button @click="result = operand1 + operand2">+</button>
-      <button @click="result = operand1 - operand2">-</button>
-      <button @click="result = operand1 / operand2">/</button>
-      <button @click="result = operand1 * operand2">*</button>
-      <button @click="result = Math.pow(operand1, operand2)">^</button>
-      <button @click="result = Math.trunc(operand1 / operand2)">:</button>
-    </div>
+    <router-view />
   </div>
 </template>
- 
-<script>
-export default {
-  name: "Calculator",
-  data() {
-    return {
-      operand1: 0,
-      operand2: 0,
-      result: 0,
-    };
-  },
-};
-</script>
 
 <style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Здесь могла быть ваша реклама" />
+    <!-- <HelloWorld msg="Здесь могла быть ваша реклама" /> -->
+    <Calculator />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Calculator from "@/components/Calculator.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Calculator,
   },
 };
 </script>
